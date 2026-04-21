f2b99827c253e67e9d08ae9a915c68b2617b5c9c
KukishKakish <154073211+KukishKakish@users.noreply.github.com>
2026-04-21 13:57:25 +0300

    Create App.jsx

---
+++ ToggleButton/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ToggleButton from "./components/ToggleButton";
+
+function App() {
+  return (
+    <div>
+      <ToggleButton />
+    </div>
+  );
+}
+
+export default App;
